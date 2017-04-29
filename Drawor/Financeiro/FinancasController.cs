668475c17075903a53beda00b0e0dd107abeb676
Lincoln Faria <lincolncardosodefaria@gmail.com>
2017-04-29 18:51:11 -0300

    --add financas controller

---
+++ Drawor/Financeiro/FinancasController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Drawor.Financeiro
+{
+    public class FinancasController : Controller
+    {
+        // GET: Financas
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult CadastroDespesa()
+        {
+            return null;
+        }
+    }
+}
